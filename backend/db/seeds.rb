da3135bcc34890198e07ffce5422e811daca3b09
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2020-01-14 16:34:09 -0500

    Update seeds.rb

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,5 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-User.destroy_all
-u = User.create(name: ENV['USER'])
+if !User.first 
+  u = User.create(name: ENV['USER'])
+end
